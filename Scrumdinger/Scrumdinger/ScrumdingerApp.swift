8baf825cd8f7dbf5c9a1caf6c296fc465abc0832
wonkwh <wonkwh@gmail.com>
2020-12-16 16:50:17 +0900

    Using Stacks to Arrange Views

---
+++ Scrumdinger/Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by wonkwh on 2020/12/16.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MeetingView()
+        }
+    }
+}
