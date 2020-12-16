0a9cd15f83de55e7e159ecaedf73bf9adf9989f0
wonkwh <wonkwh@gmail.com>
2020-12-16 20:29:24 +0900

    add ListView

---
+++ Scrumdinger/Scrumdinger/ScrumsView.swift
@@ -0,0 +1,26 @@
+//
+//  ScrumsView.swift
+//  Scrumdinger
+//
+//  Created by wonkwh on 2020/12/16.
+//
+
+import SwiftUI
+
+struct ScrumsView: View {
+    let scrums: [DailyScrum]
+    var body: some View {
+        List {
+            ForEach(scrums) { scrum in
+                CardView(scrum: scrum)
+                    .listRowBackground(scrum.color)
+            }
+        }
+    }
+}
+
+struct ScrumsView_Previews: PreviewProvider {
+    static var previews: some View {
+        ScrumsView(scrums: DailyScrum.data)
+    }
+}
