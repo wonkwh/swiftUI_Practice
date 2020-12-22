8dde1a2bec60376d6b89bcdc5217e77214737093
wonkwh <wonkwh@gmail.com>
2020-12-22 20:16:53 +0900

    add scrum history

--- Scrumdinger/Scrumdinger/MeetingView.swift
+++ Scrumdinger/Scrumdinger/MeetingView.swift
@@ -38,6 +38,8 @@ struct MeetingView: View {
         }
         .onDisappear() {
             scrumTimer.stopScrum()
+            let newHistory = History(attendees: scrum.attendees, lengthInMinutes: scrum.lengthInMinutes)
+            scrum.history.insert(newHistory, at: 0)
         }
     }
 }
