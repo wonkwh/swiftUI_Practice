e9ffcd016c149aec7024c5f4b5c900317bed2167
wonkwh <wonkwh@gmail.com>
2020-12-28 10:37:31 +0900

    draw the progress ring

--- Scrumdinger/Scrumdinger/MeetingTimerView.swift
+++ Scrumdinger/Scrumdinger/MeetingTimerView.swift
@@ -7,6 +7,31 @@
 
 import SwiftUI
 
+struct SpeakerArc: Shape {
+    let speakerIndex: Int
+    let totalSpeakers: Int
+    
+    private var degreesPerSpeaker: Double {
+        360.0 / Double(totalSpeakers)
+    }
+    private var startAngle: Angle {
+        Angle(degrees: degreesPerSpeaker * Double(speakerIndex) + 1.0)
+    }
+    private var endAngle: Angle {
+        Angle(degrees: startAngle.degrees + degreesPerSpeaker - 1.0)
+    }
+    
+    func path(in rect: CGRect) -> Path {
+        let diameter = min(rect.size.width, rect.size.height) - 24.0
+        let radius = diameter / 2.0
+        let center = CGPoint(x: rect.origin.x + rect.size.width / 2.0,
+                             y: rect.origin.y + rect.size.height / 2.0)
+        return Path { path in
+            path.addArc(center: center, radius: radius, startAngle: startAngle, endAngle: endAngle, clockwise: false)
+        }
+    }
+}
+
 struct MeetingTimerView: View {
     @Binding var speakers: [ScrumTimer.Speaker]
     var scrumColor: Color
@@ -24,6 +49,15 @@ struct MeetingTimerView: View {
             }
             .accessibilityElement(children: .combine) //두문장을 하나로
             .foregroundColor(scrumColor.accessibleFontColor)
+            
+            ForEach(speakers) { speaker in
+                if speaker.isCompleted,
+                   let index = speakers.firstIndex(where: {$0.id == speaker.id}) {
+                    SpeakerArc(speakerIndex: index, totalSpeakers: speakers.count)
+                        .rotation(Angle(degrees: -90))
+                        .stroke(scrumColor, lineWidth: 12)
+                }
+            }
         }
         .padding(.horizontal)
     }
