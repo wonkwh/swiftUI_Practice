110b0235b25ca82af5bbb5bc7a0f51080e737283
wonkwh <wonkwh@nc-and.com>
2024-01-31 20:54:46 +0900

    ✨ add scroll section

--- discovery/Targets/DiscoveryUI/Sources/ContentView.swift
+++ discovery/Targets/DiscoveryUI/Sources/ContentView.swift
@@ -5,28 +5,115 @@ public struct ContentView: View {
 
     public var body: some View {
       NavigationView {
-        DiscoveryCategoryView()
+        ScrollView {
+          DiscoveryCategoryView()
+          PopularDestinationView()
+          PopularPlacesToEatView()
+          TrendingCreatorsView()
+        }
         .navigationTitle("Discovery")
       }
     }
 }
 
+
+struct PopularDestinationView: View {
+  var body: some View {
+    VStack {
+      HStack {
+        Text("Popular Destinations")
+          .font(.system(size: 14, weight: .semibold))
+        Spacer()
+        Text("See All")
+          .font(.system(size: 12, weight: .semibold))
+      }.padding(.horizontal).padding(.top)
+
+      ScrollView(.horizontal, showsIndicators: false) {
+        HStack(spacing: 8.0) {
+          ForEach(0..<5, id: \.self) { num in
+            Rectangle()
+              .fill(Color.gray)
+              .frame(width: 125, height: 160)
+              .cornerRadius(10)
+              .shadow(radius: 4)
+              .padding(.bottom)
+          }
+        }.padding(.horizontal)
+      }
+    }
+  }
+}
+
+struct PopularPlacesToEatView: View {
+  var body: some View {
+    VStack {
+      HStack {
+        Text("Popular Place To Eat")
+          .font(.system(size: 14, weight: .semibold))
+        Spacer()
+        Text("See All")
+          .font(.system(size: 12, weight: .semibold))
+      }.padding(.horizontal).padding(.top)
+
+      ScrollView(.horizontal, showsIndicators: false) {
+        HStack(spacing: 12.0) {
+          ForEach(0..<5, id: \.self) { num in
+            Rectangle()
+              .fill(Color.gray)
+              .frame(width: 300, height: 120)
+              .cornerRadius(10)
+              .shadow(radius: 4)
+              .padding(.bottom)
+          }
+        }.padding(.horizontal)
+      }
+    }
+  }
+}
+
+struct TrendingCreatorsView: View {
+  var body: some View {
+    VStack {
+      HStack {
+        Text("Trending Creator")
+          .font(.system(size: 14, weight: .semibold))
+        Spacer()
+        Text("See All")
+          .font(.system(size: 12, weight: .semibold))
+      }.padding(.horizontal).padding(.top)
+
+      ScrollView(.horizontal, showsIndicators: false) {
+        HStack(spacing: 12.0) {
+          ForEach(0..<12, id: \.self) { num in
+            Rectangle()
+              .fill(Color.gray)
+              .frame(width: 50, height: 50)
+              .cornerRadius(25)
+              .shadow(radius: 4)
+              .padding(.bottom)
+          }
+        }.padding(.horizontal)
+      }
+    }
+  }
+}
+
 struct DiscoveryCategoryView: View {
   var body: some View {
-    ScrollView(.horizontal) {
-      HStack(spacing: 16) {
+    ScrollView(.horizontal, showsIndicators: false) {
+      HStack(spacing: 8) {
         ForEach(0..<5, id: \.self) { num in
           VStack(spacing: 4) {
             Spacer().frame(width: 50, height: 50)
               .background(Color.gray)
               .cornerRadius(.infinity)
               .shadow(radius: 5)
-            Text("Artss")
+            Text("Art")
               .font(.system(size: 12, weight: .semibold))
           }
         }
-      }
-      Spacer()
+      }.padding(.horizontal)
+//      Spacer()
     }
   }
 }
